fb4fe073605c531faf9060b5ec8bda293a56a717
Katyassha <113689243+Katyassha@users.noreply.github.com>
2022-11-06 11:52:27 +0700

    Update SR2 №3 (19).py

--- SR2 №3 (19).py
+++ SR2 №3 (19).py
@@ -3,6 +3,6 @@ a = float(input('Левая граница: '))
 b = float(input('Правая граница: '))
 h = float(input('Задайте величину шага: '))
 while a <= b:
-    y = round(((math.exp(a) - (math.exp(a)) ** -1) / 2) ** 3, 3)
+    y = round(((math.exp(a) - math.exp(-a)) / 2) ** 3, 3)
     print('x = ', a, '; y = ', y, sep='')
     a = round(a + h, 3)
