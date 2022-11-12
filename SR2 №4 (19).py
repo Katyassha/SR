1542ddcdff4807a1c77d07ed1fb7e27162897ea3
Katyassha <113689243+Katyassha@users.noreply.github.com>
2022-11-12 17:49:13 +0700

    Delete SR2 №4 (19).py

--- SR2 №4 (19).py
+++
@@ -1,7 +0,0 @@
-import math
-s = 0
-b = int(input('Введите количество значений: '))
-for i in range(1, b + 1):
-    n = float(input())
-    s += (math.cos(math.radians(n + 1)) / (2 * n))
-print('Сумма =', round(s, 3))
